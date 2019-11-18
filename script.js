f99fa297b650de7b0f1d32c125bf747857c1d6fd
kjenkins5678 <kajenkins9@gmail.com>
2019-11-18 17:28:47 -0500

    added very basic prompts for password characteristics

---
+++ script.js
@@ -0,0 +1,14 @@
+var passLength = prompt("Choose a password length (between 8 and 128): ");
+console.log(passLength)
+
+var charSpecial = prompt("Type your special characters: ")
+console.log(charSpecial)
+
+var charNum = prompt("Type your numeric characters:" )
+console.log(charNum)
+
+var charLower = prompt("Type your lowercase characters:" )
+console.log(charLower)
+
+var charUpper = prompt("Type your uppercase characters: ")
+console.log(charUpper)
