2baf6e9f55b5be9dc026bb67866d85f4b7409354
kjenkins5678 <kajenkins9@gmail.com>
2019-11-19 17:34:28 -0500

    when button clicked, get text and return the text in the text box

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.getElementById("generatePW").onclick = function(){
     console.log("button clicked")
-    var someText = prompt("Type some text: ")
 
+    var someText = prompt("Type some text: ")
+    var textArea = document.getElementById("textArea")
     
+    textArea.textContent = someText;
 };
