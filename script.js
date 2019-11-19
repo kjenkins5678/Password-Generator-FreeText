bc81f1b2c67722d29bae904720b3096d3f678852
kjenkins5678 <kajenkins9@gmail.com>
2019-11-19 17:15:54 -0500

    added code to write to console when button is clicked

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-var passLength = prompt("Choose a password length (between 8 and 128): ");
-console.log(passLength)
+document.getElementById("generatePW").onclick = function(){
+    console.log("button clicked")
+    var someText = prompt("Type some text: ")
 
-var charSpecial = prompt("Type your special characters: ")
-console.log(charSpecial)
-
-var charNum = prompt("Type your numeric characters:" )
-console.log(charNum)
-
-var charLower = prompt("Type your lowercase characters:" )
-console.log(charLower)
-
-var charUpper = prompt("Type your uppercase characters: ")
-console.log(charUpper)
+    
+};
