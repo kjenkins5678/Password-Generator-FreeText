fa4209fc3722b0835d3a3cea07113fdd0eee8d78
kjenkins5678 <kajenkins9@gmail.com>
2019-11-21 21:09:23 -0500

    changed to single select from multiselect

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-var pwdLenSelector = document.querySelector("#FormControlSelect");
+//Generate password length options
+var pwLenSelector = document.querySelector("#FormControlSelect");
 
 for (i = 8; i < 129; i++)
 {
     var option = document.createElement("option");
     option.innerHTML = i;
-    pwdLenSelector.appendChild(option);
+    pwLenSelector.appendChild(option);
 };
 
+var pwGen = document.querySelector("#generatePW");
+var pwLen = pwLenSelector.options[pwLenSelector.selectedIndex].value;
 
+pwGen.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log(pwLen);
+
+    
+});
 
