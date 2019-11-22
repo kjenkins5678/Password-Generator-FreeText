394718cbe2f35b5c6853fd9830a7eb32a370c276
kjenkins5678 <kajenkins9@gmail.com>
2019-11-21 19:57:48 -0500

    used javascript to generate the password length options

--- script.js
+++ script.js
@@ -1,54 +1,11 @@
-document.getElementById("generatePW").onclick = function(){
-    console.log("button clicked")
+var pwdLenSelector = document.querySelector("#FormControlSelect");
 
-    /* 
-    Pseudocode
-
-    know the password length
-    know which of the following are populated: charSpecial, charNum, chatLower, charUpper
-    validate users responses to above questions
-    at least one of char variables must be populated
-
-    ##Getting the Password Length right with all provided Char Types##
-    count how many characters the user has provided between all the chars
-    if too many
-        only select a portion of each
-
-    if too few
-        duplicate the chars until the string is long enough
-
-    if exactly the right length
-        move on the next step
-
-    ##Stitch together a random password
-    for the chars that are populated
-        randomly choose an order to concatenate them
-    */
-
-    //##Code to prompt for user responses##
-
-    var passLength = prompt("Choose a password length (between 8 and 128): ");
-    var charSpecial = prompt("Type your special characters: ");
-    var charNum = prompt("Type your numeric characters:" );
-    var charLower = prompt("Type your lowercase characters:" );
-    var charUpper = prompt("Type your uppercase characters: ");
-
-    var pwdChar = [charSpecial, charNum, charLower, charUpper];
-
-    // var totalCharLength = charSpecial.length + charNum.length + charLower.length + charUpper.length;
-    for (i = 0; i < pwdChar.length; i++){
-        console.log(pwdChar[i]);
-        if (!pwdChar[i]){
-            console.log("we have a blank")
-            pwdChar.splice(i, 1);
-        }
-        console.log(pwdChar.length)
-
-    } 
-    
-
-    // var someText = prompt("Type some text: ")
-    // var textArea = document.getElementById("textArea")
-
-    // textArea.textContent = someText;
+for (i = 8; i < 129; i++)
+{
+    var option = document.createElement("option");
+    option.innerHTML = i;
+    pwdLenSelector.appendChild(option);
 };
+
+
+
