bba944ef2c3a7196695561a0c080f9c38c49463b
kjenkins5678 <kajenkins9@gmail.com>
2019-11-23 23:26:21 -0500

    added length changing components

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ var alphabetChar = "abcdefghijklmnopqrstuvwxyz".split("");
 var alphabetUChar = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
 var numericChar = "1234567890".split("");
 var specialChar = "!@#$%^&*()_+-={}|\\][;'\":<>?/.,~`".split("");
+var textbox = document.getElementById("textArea");
+var copybtn = document.getElementById("copy");
 
 function validateInput()
 {
@@ -89,8 +91,91 @@ function validateInput()
         }
     }
     }
-
 };
+
+function randomizer ()
+{
+    var popFields = [];
+    var randomArray = [];
+
+    if (!specChar.value == "")
+    {
+        popFields.push(specChar.value)
+    }
+
+    if (!numChar.value == "")
+    {
+        popFields.push(numChar.value)
+    }
+
+    if (!ucChar.value == "")
+    {
+        popFields.push(ucChar.value)
+    }
+
+    if (!lcChar.value == "")
+    {
+        popFields.push(lcChar.value)
+    }
+
+    do
+    {
+        var randomNum = Math.floor(Math.random() * popFields.length);
+        randomArray.push(popFields[randomNum]);
+        popFields.splice(randomNum, 1);
+        console.log(popFields);
+    }
+    while (popFields.length > 0);
+
+    var randomString = randomArray.join("")
+    return randomString;
+};
+
+function fixLength()
+{
+    var result = randomizer();
+    if (result.length == pwLen)
+    {
+        textbox.innerHTML=result;
+    }
+
+    if (result.length < pwLen)
+    {
+        var short = pwLen - result.length;
+        if (short < pwLen)
+        {
+            result = result + result.slice(0, short + 1);
+            textbox.innerHTML=result;
+        }
+
+        // if (short > pwLen)
+        // {
+
+        // }
+    }
+
+    if (result.length > pwLen)
+    {
+        console.log("greater")
+        result = result.slice(0, pwLen + 1);
+        textbox.innerHTML=result;
+    }
+
+}
+
+// function copy2clip() { 
+//     var copyText = textbox.value
+
+//     /* Select the text field */
+//     copyText.select();
+//     copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  
+//     /* Copy the text inside the text field */
+//     document.execCommand("copy");
+  
+//     /* Alert the copied text */
+//     alert("Copied the text: " + copyText);
+//   }
 
 pwGen.addEventListener("click", function(event){
     event.preventDefault();
@@ -100,5 +185,8 @@ pwGen.addEventListener("click", function(event){
     console.log(numChar.value);
     console.log(ucChar.value);
     console.log(lcChar.value);
+    fixLength();
 });
+
+
 
